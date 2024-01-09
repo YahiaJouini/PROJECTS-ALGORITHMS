ef6c456f3563b686bc4b639e068c9d3426108186
YahiaJouini <jouiniyahya117@gmail.com>
2024-01-09 12:02:29 +0100

    hashing the passwords

--- Solo Project Proposal/bright_ideas/server/models/user.model.js
+++ Solo Project Proposal/bright_ideas/server/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const { isEmail } = require("validator")
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,14 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Password must be at least 6 characters long']
 
     }
+})
+
+// fire a function before a doc is saved to the db
+// we use function instead of an arrow key to get the value of this (instant of the user created)
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 module.exports = mongoose.model('user', userSchema)
